f4002fe728b1c3650ac6ab6408128ae26a84ba87
VaibhavAgarwal452 <vaibhavagarwal78@gmail.com>
2024-06-28 21:46:35 +0530

    Added testing route will refactor later

--- src/app.js
+++ src/app.js
@@ -20,6 +20,9 @@ import QuotesRouter from "./routes/quote.routes.js"
 import CollectionRouter from "./routes/collection.routes.js"
 
 //routes declaration
+app.get("/", (req, res) => {
+    res.send("working")
+})
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/quotes", QuotesRouter)
 app.use("/api/v1/collections", CollectionRouter)
