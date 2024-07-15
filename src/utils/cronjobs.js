7ed4441dbc193d2f8dca3d1f5f866ed20c556f8c
VaibhavAgarwal452 <vaibhavagarwal78@gmail.com>
2024-07-15 21:36:53 +0530

    testing cron jobs on server

--- src/utils/cronjobs.js
+++ src/utils/cronjobs.js
@@ -5,7 +5,8 @@ import { sendNotificationToAllUsers } from './commonFunctions.js';
 export const cronjobs = async () => {
     // const users = await User.find({})
 
-    cron.schedule('0 12,17 * * *', async () => {
+    cron.schedule('*/2 * * * *', async () => {
+        console.log("it runs")
         const currentQuote = await Quote.aggregate([{ $sample: { size: 1 } }])
         sendNotificationToAllUsers(currentQuote[0].quote)
 
